0e27e34fcbfddddb7b75a790e6ab1cfeba7c6778
pratik <bhurepratik2@gmail.com>
2023-12-21 23:43:18 +0530

    largest and smallest element

---
+++ Array/LargestAndSmallestElement.java
@@ -0,0 +1,39 @@
+package array;
+
+import java.util.Scanner;
+
+public class LargestAndSmallestElement {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int i,j,temp,n;
+		System.out.println("enter size of array");
+		n=sc.nextInt();
+		int[] arr=new int[n];
+		n=n-1;
+		System.out.println("enter "+n+" element");
+		for(i=0;i<arr.length;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		for(i=0;i<arr.length-1;i++)
+		{
+			for(j=0;j<arr.length-1-i;j++)
+			{
+				if(arr[j]<arr[j+1])
+				{
+					temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+				}
+			}
+		}
+		for(i=0;i<arr.length;i++)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println("largest element="+arr[0]);
+		System.out.println("largest element="+arr[arr.length]);
+	}
+
+}
