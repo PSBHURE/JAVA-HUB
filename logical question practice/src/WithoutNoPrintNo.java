f134482ea973ccf492afd83bf1cbaeabf348af79
pratik <bhurepratik2@gmail.com>
2023-12-22 10:19:40 +0530

    printing 1-100 without using any number

---
+++ logical question practice/src/WithoutNoPrintNo.java
@@ -0,0 +1,14 @@
+
+public class WithoutNoPrintNo {
+
+	public static void main(String[] args) {
+		String x="..........";
+		int one='A'/'A';
+		System.out.println(one);
+		for(int i=one;i<=x.length()*x.length();i++)
+		{
+			System.out.print(i+" ");
+		}
+	}
+
+}
