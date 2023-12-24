c0cf5763b33adc7b581a6f88d4aea78bb695afc9
pratik <bhurepratik2@gmail.com>
2023-12-24 11:06:33 +0530

    insert value into array it will automatically takes its position in sorted array

--- Array/InsertValueInSortedArray.java
+++ Array/InsertValueInSortedArray.java
@@ -14,15 +14,43 @@ The exist array list is :
 2 5 7 9 11
 After Insert the list is :
 2 5 7 8 9 11*/
+/*Write a program in JAVA to insert the values in the array (sorted list).
+Test Data :
+Insert New value in the sorted array :
+-----------------------------------------
+Input the size of array : 5
+Input 5 elements in the array in ascending order:
+element - 0 : 2
+element - 1 : 5
+element - 2 : 7
+element - 3 : 9
+element - 4 : 11
+Input the value to be inserted : 8
+The exist array list is :
+2 5 7 9 11
+After Insert the list is :
+2 5 7 8 9 11*/
 package array;
 
 import java.util.Scanner;
 
-public class InsertValueInSortedArray {
-	public static int[] AddNewElementInSortedArray(int[] arr,int e)
+public class InsertValueInArrayAndSortIt {
+	public static int[] Insert(int[] arr,int e)
 	{
-		int [] NewArr=new int[arr.length+1];
-		int i,j;
+		int[] NewArr=new int[arr.length+1];
+		int i,j,temp;
+		for(i=0;i<arr.length-1;i++)
+		{
+			for(j=0;j<arr.length-1-i;j++)
+			{
+				if(arr[j]>arr[j+1])
+				{
+					temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j]=temp;
+				}
+			}
+		}
 		for(i=0;i<arr.length && arr[i]<e;i++)
 		{
 			NewArr[i]=arr[i];
@@ -34,24 +62,26 @@ public class InsertValueInSortedArray {
 		}
 		return NewArr;
 	}
+
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int i,size,e;
 		int[] arr;
+		int size,i,e;
 		System.out.println("enter size of array");
 		size=sc.nextInt();
 		arr=new int[size];
-		System.out.println("enter"+size+" elements");
+		System.out.println("enter "+size+" elements in array");
 		for(i=0;i<size;i++)
 		{
 			arr[i]=sc.nextInt();
 		}
 		System.out.println("enter new element");
 		e=sc.nextInt();
-		int[] arr1=AddNewElementInSortedArray(arr, e);
-		for(int x:arr1)
+		int[] NewArr=Insert(arr, e);
+		for(int x:NewArr)
 		{
 			System.out.print(x+" ");
 		}
 	}
+
 }
