57f3f00d7ceb0de690fbe97bf1036d7e2d708564
pratik <bhurepratik2@gmail.com>
2024-02-06 21:40:34 +0530

    max till i

--- Top Array Question/src/MaxElementTillN.java
+++ Top Array Question/src/MaxElementTillN.java
@@ -7,17 +7,19 @@ public class MaxElementTillN {
 		Scanner sc=new Scanner(System.in);
 		int arr[]= {1,2,-4,5,9,8,10};
 		System.out.println("enter value till index:");
-		int n=sc.nextInt();
-		int max=FindMaxTillIndexN(arr,n);
+		System.out.println();
+		int max=FindMaxTillIndexN(arr);
+		System.out.println();
 		System.out.println(max);
 	}
-	public static int FindMaxTillIndexN(int[] arr,int n)
+	public static int FindMaxTillIndexN(int[] arr)
 	{
 		int max=0;
-		for(int i=0;i<n;i++)
+		for(int i=0;i<arr.length;i++)
 		{
 			if(arr[i]>max)
 			max=arr[i];
+			System.out.print(max+" ");
 		}
 		return max;
 	}
