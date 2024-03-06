31eba5747b75bf49a417e5b05fda5692d2595fcc
pratik <bhurepratik2@gmail.com>
2024-03-06 15:15:19 +0530

    function of printling string

---
+++ STRING/PrintString.java
@@ -0,0 +1,11 @@
+public class PrintString
+{
+public static void main(String[] args)
+{
+	PRINTSTRING("Welcome, w3resource" );
+}
+public static void PRINTSTRING(String str)
+{
+	System.out.println("The string you entered is :"+str);
+}
+}
