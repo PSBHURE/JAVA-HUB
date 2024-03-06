8e68bba9c9984c6e0af5acec80f27a0fc543adbe
pratik <bhurepratik2@gmail.com>
2024-03-06 16:01:16 +0530

    seprate the character in string

--- STRING/SeprateString.java
+++ STRING/SeprateString.java
@@ -3,17 +3,15 @@ public class SeprateString
 	public static void main(String[] args)
 	{
 		String str="w3resource.com";
-		//String str2=StringSepration(str);
 		StringSepration(str);
-		//System.out.printlm(str2);
 	}
 	public static void StringSepration(String str)
 	{
-	char[] arr=str.toCharArray();
-	for(char i:arr)
-	{
-	System.out.print(i+" ");
-	}
-	}
+		char[] arr=str.toCharArray();
+		for(int i=0;i<arr.length;i++)
+		{
+		System.out.print(arr[i]+" ");
+		}
 	
+	}
 }
