38d672bb9932bc343c88d6389d7a08aa2f542bca
pratik <bhurepratik2@gmail.com>
2024-03-07 16:29:58 +0530

    count number of words in a string

--- STRING/StringChecking.java
+++ STRING/StringChecking.java
@@ -1,4 +1,4 @@
-public class StringChecking
+/*public class StringChecking
 {
 	public static void main(String[] args)
 	{
@@ -15,5 +15,29 @@ public class StringChecking
 		
 		else
 		return false;
+	}
+}*/
+
+
+
+public class StringChecking
+{
+	public static void main(String[] args)
+	{
+	boolean flag=(StringEquals("PRATIK","PRATIK"));
+	System.out.println(flag);
+	}
+	public static boolean StringEquals(String s1,String s2)
+	{
+		char[] arr1=s1.toCharArray();
+		char[] arr2=s2.toCharArray();
+		if(arr1.length!=arr2.length)
+			return false;
+		for(int i=0;i<arr1.length-1;i++)
+		{
+			if(arr1[i]!=arr2[i])
+				return false;
+		}
+		return true;
 	}
 }
