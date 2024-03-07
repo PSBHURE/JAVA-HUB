714d17df8207eb41b4331bf5a70f4155e620ec50
pratik <bhurepratik2@gmail.com>
2024-03-07 15:56:30 +0530

    Finding frequency of lement in an array

--- Array/FindFrequencyOfElement.java
+++ Array/FindFrequencyOfElement.java
@@ -3,8 +3,8 @@ public class FindFrequencyOfElement
 {
 	public static void main(String[] args)
 	{
-		int[] arr={1,2,3,4,5,1,2,3,4,1};
-		Map<Integer,Integer> map=FindFrequency(arr);
+		int[] arr={1,2,3,4,2,3,2,5,7,8,1,2,8,0,8,6,8};
+		Map<Integer,Integer>map=FindFrequency(arr);
 		for(Map.Entry<Integer,Integer>entry:map.entrySet())
 		{
 			System.out.println(entry.getKey()+" -> "+entry.getValue());
@@ -12,11 +12,11 @@ public class FindFrequencyOfElement
 	}
 	public static Map<Integer,Integer> FindFrequency(int[] arr)
 	{
-		Map<Integer,Integer>map=new HashMap<>();
-		for(int num:arr)
-		{
-			map.put(num,map.getOrDefault(num,0)+1);
-		}
-		return map;
+			Map<Integer,Integer>map=new HashMap<>();
+			for(int element:arr)
+			{
+				map.put(element,map.getOrDefault(element,0)+1);
+			}
+			return map;
 	}
 }
