59e894b07f86698a467d902786aa2612e4c7530f
pratik <bhurepratik2@gmail.com>
2024-03-08 13:28:45 +0530

    maximum sum of array upto perticular index

--- Array/MaxSum.java
+++ Array/MaxSum.java
@@ -3,22 +3,20 @@ public class MAXSUM
 	public static void main(String[] args)
 	{
 		int[] arr={1,9,-1,-2,7,3,-1,2};
-		System.out.println(MaxSum(arr,4));
+		System.out.print(MaxSum(arr,4));
 	}
 	public static int MaxSum(int[] arr,int k)
 	{
-		 int msum=0,wsum=0;
-		for(int i=0;i<k;i++)
-		{
-		 wsum=wsum+arr[i];
-		}
-		for(int end=k;end<arr.length;end++)
-		{
-		 wsum+=arr[end]-arr[end-k];	
-		 if(wsum>msum)
-		msum=wsum;
-		}
-		
-		return msum;
+	 int wsum=0,msum=0;
+	 for(int i=0;i<k;i++)
+	 {
+		wsum+=arr[i];
+	 }
+	 for(int end=k;end<arr.length;end++)
+	 {
+		wsum+=arr[end]-arr[end-k];
+		msum=Math.max(wsum,msum);
+	 }
+	 return msum;
 	}
 }
