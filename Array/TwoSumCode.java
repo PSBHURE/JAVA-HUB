c0ce9e0240908ff6e6623ec784272452b3178ad0
pratik <bhurepratik2@gmail.com>
2024-03-08 16:02:58 +0530

    TWO SUM code of leadcode

---
+++ Array/TwoSumCode.java
@@ -0,0 +1,34 @@
+public class TwoSumCode
+{
+	public static void main(String[] args)
+	{
+		int arr[]={-3,2,3,3,6,8,15};
+		int target=6;
+		int arr1[]=Two_Sum(arr,target);
+		for(int i=0;i<arr1.length;i++)
+		{
+			System.out.print(arr1[i]+" ");
+		}
+	}
+	public static int[] Two_Sum(int[] arr,int sum)
+	{
+		int begin=0,end=arr.length-1,sum1=0;
+		int[] arr1=new int[2];
+		while(begin<end)
+		{
+			sum1=arr[begin]+arr[end];
+			if(sum1>sum)
+			end--;
+			if(sum1<sum)
+			begin++;
+			else
+			if(sum1==sum)
+			{
+			arr1[0]=begin;
+			arr1[1]=end;
+			break;
+			}
+		}
+		return arr1;
+	}
+}
