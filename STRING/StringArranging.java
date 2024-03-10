a6e41bcbf6abeafa70a690e635ccad785f336a76
pratik <bhurepratik2@gmail.com>
2024-03-10 12:19:26 +0530

    Sorting the string as per ascii value

---
+++ STRING/StringArranging.java
@@ -0,0 +1,26 @@
+public class StringArranging
+{
+	public static void main(String[] args)
+	{
+		String str="www.w3resource.com";
+		System.out.println(StringSortation(str));
+	}
+	public static String StringSortation(String str)
+	{
+		char[] arr=str.toCharArray();
+		char temp='\0';
+		for(int i=0;i<arr.length;i++)
+		{
+			for(int j=i+1;j<arr.length;j++)
+			{
+				if((int)arr[i]>(int)arr[j])
+				{
+			 	temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
+				}
+			}
+		}
+		return String.valueOf(arr);
+	}
+}//3ceeorrsuw 
