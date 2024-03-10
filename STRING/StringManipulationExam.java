aae5cc02f488ccd5b12a86bd56d89871aa56fac4
pratik <bhurepratik2@gmail.com>
2024-03-10 11:57:59 +0530

    increae each character of string by perticular no

--- STRING/StringManipulationExam.java
+++ STRING/StringManipulationExam.java
@@ -9,9 +9,14 @@ public class StringManipulationExam
 	public static String Modify(String str,int shift)
 	{
 		char[] arr=str.toCharArray();
+		char base='\0';
 		for(int i=0;i<arr.length;i++)
 		{
-		 arr[i]=arr[i]+shift;
+			if((arr[i]>='a'&&arr[i]<='z')||(arr[i]>='A'&&arr[i]<='Z'))
+			{
+				base=Character.isLowerCase(arr[i])?'a':'A';
+				arr[i]=(char)(base+(arr[i]-base+shift)%26);
+			}
 		}
 		return String.valueOf(arr);
 	}
