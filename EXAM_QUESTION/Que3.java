d0411023c2fe2a679b68c7ce6ec417d0f2e9e3c6
pratik <bhurepratik2@gmail.com>
2024-03-14 23:46:53 +0530

    Que4

--- EXAM_QUESTION/Que3.java
+++ EXAM_QUESTION/Que3.java
@@ -8,7 +8,7 @@ public class Que3
 	public static int Difference(int no)
 	{
 		List<Integer>list=new ArrayList<>();
-		int r=0;
+		int r=0,i=0,max=0,min=0;
 		while(no>0)
 		{
 			r=no%10;
@@ -16,36 +16,19 @@ public class Que3
 			no=no/10;
 		}
 		int[] arr=new int[list.size()];
-		int i=0;
 		for(int X:list)
 		{
 			arr[i]=X;
 			i++;
 		}
 		Arrays.sort(arr);
-		int min=0;
-		for(int m=0;m<arr.length;m++)
+		for(int j=0;j<arr.length;j++)
 		{
-			min=min*10+arr[m];
+			min=min*10+arr[j];
 		}
-		int temp=min;
-		list.clear();
-		while(temp>0)
+		for(int k=arr.length-1;k>=0;k--)
 		{
-			r=temp%10;
-			list.add(r);
-			temp=temp/10;
-		}
-		int j=0;
-		for(int X:list)
-		{
-			arr[j]=X;
-			j++;
-		}
-		int max=0;
-		for(int z=0;z<arr.length;z++)
-		{
-			max=max*10+arr[z];
+			max=max*10+arr[k];
 		}
 		return max-min;
 	}
